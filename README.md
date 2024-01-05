939cfb0d3c2adc7f73d2a8726ba9ad511ee4acd4
basium <cristiano.alessio@gmail.com>
2024-01-05 17:56:24 +0100

    Project Created

--- README.md
+++
@@ -1,2 +0,0 @@
-# CarRentalAssignment
-A Nuxt, Tailwind and Pinia, car rental application built as part of an interview process. 
---
+++ dm-car-rental/components/HelloComponent.vue
@@ -0,0 +1,8 @@
+<template>
+    Hello Component, from {{ name }} {{ result?.length }}
+</template>
+<script setup lang="ts">
+    const name ="alessio",
+    apiURL = "https://dm-assignment-commonshare.koyeb.app/api/cars";
+
+</script>
